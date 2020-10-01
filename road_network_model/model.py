c756a010dee7cdf0245c8a4f5b5d518c675903e7
Albert Darmawan <darmawanalbert@gmail.com>
2020-10-01 18:11:28 +0700

    Implement main modules (#11)
    * Checkpoint 1
    
    * Checkpoint 2

--- road_network_model/model.py
+++ road_network_model/model.py
@@ -1,30 +1,64 @@
-"""
-Wolf-Sheep Predation Model
-================================
-
-Replication of the model found in NetLogo:
-    Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
-    http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
-    Center for Connected Learning and Computer-Based Modeling,
-    Northwestern University, Evanston, IL.
-"""
-
 from mesa import Model
 from mesa.space import MultiGrid
+from mesa.time import SimultaneousActivation
 from mesa.datacollection import DataCollector
 
-from road_network_model.agents import Car
+from road_network_model.agent import Car, Road, Building
+from road_network_model.map import MapGenerator
+from road_network_model.constant import DIRECTION, CAR_STATE
 
-
-class Car(Model):
- 
+class RoadNetworkModel(Model):
     description = (
-        "A model for simulating wolf and sheep (predator-prey) ecosystem modelling."
+        "A model for simulating Road Network Model"
     )
 
+    def __init__(self, number_of_cars, width, height):
+        # Set up Spatial dimension
+        self.grid = MultiGrid(width, height, True)
+
+        # Set up Temporal dimension
+        self.schedule = SimultaneousActivation(self)
+
+        ## generate road
+        self.map = MapGenerator()
+        roadPosition = self.map.get_road_position()
+        for i in range(len(roadPosition)):
+            road = Road(i, roadPosition[i], self)
+            self.grid.place_agent(road, roadPosition[i])
+
+        ## generate building
+        buildingPosition = self.map.get_building_position()
+        for i in range(len(buildingPosition)):
+            building = Building(i, buildingPosition[i], self)
+            self.grid.place_agent(building, buildingPosition[i])
+
+        # Create a set of initial car positions
+        initial_car_position = set()
+        while len(initial_car_position) != number_of_cars:
+            random_road_index = self.random.randint(0, len(roadPosition) - 1)
+            initial_car_position.add(random_road_index)
+
+        layout = self.map.get_layout()
+        for i in range (number_of_cars):
+            plate_number_oddity = self.random.randint(0, 1)
+            current_index = initial_car_position.pop()
+            source_x = roadPosition[current_index][0]
+            source_y = roadPosition[current_index][1]
+            destination_x = 0
+            destination_y = 0
+            max_speed = 100
+            car_direction = layout[source_x][source_y] # "^" "v" ">" "<"
+            car_state = "IDLE"
+
+            car = Car(i, plate_number_oddity,
+                        [source_x,source_y],
+                        (destination_x,destination_y),
+                        max_speed,
+                        car_direction,
+                        car_state, self)
+
+            self.grid.place_agent(car, (source_x,source_y))
+
+
     def step(self):
-        """ step """
-
-    def run_model(self, step_count=200):
-        """ run model """
-
+        self.schedule.step()
---
+++ road_network_model/portrayal.py
@@ -0,0 +1,46 @@
+from road_network_model.agent import Car, Road, Building
+
+def road_network_model_portrayal(agent):
+    """
+    This function is registered with the visualization server to be called
+    each tick to indicate how to draw the car in its current state.
+    :param agent:  the car in the simulation
+    :return: the portrayal dictionary.
+    """
+    if agent is None:
+        return
+
+    portrayal = {}
+
+    if type(agent) is Car:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "red"
+        portrayal["scale"] = 1
+        portrayal["Layer"] = 10
+        portrayal["w"] = 0.5
+        portrayal["h"] = 0.5
+        portrayal["x"] = agent.current_coor[0]
+        portrayal["y"] = agent.current_coor[1]
+        portrayal["Filled"] = "true"
+    if type(agent) is Road:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "grey"
+        portrayal["scale"] = 1
+        portrayal["Layer"] = 1
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["x"] = agent.pos[0]
+        portrayal["y"] = agent.pos[1]
+        portrayal["Filled"] = "true"
+    if type(agent) is Building:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "blue"
+        portrayal["scale"] = 1
+        portrayal["Layer"] = 1
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["x"] = agent.pos[0]
+        portrayal["y"] = agent.pos[1]
+        portrayal["Filled"] = "true"
+
+    return portrayal
