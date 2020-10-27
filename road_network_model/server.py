3da80848ee674aafcbe6ca0b3415a2f382dc7284
Albert Darmawan <darmawanalbert@gmail.com>
2020-10-27 21:04:50 +0700

    Add timer for better visualization

--- road_network_model/server.py
+++ road_network_model/server.py
@@ -1,13 +1,13 @@
 from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
 from mesa.visualization.UserParam import UserSettableParameter
-from mesa.visualization.modules import TextElement
-
 
 from road_network_model.agent import Car, TrafficLight
 from road_network_model.model import RoadNetworkModel
 from road_network_model.portrayal import road_network_model_portrayal
 from road_network_model.constant import PROJECT_TITLE, CANVAS_WIDTH, CANVAS_HEIGHT, GRID_WIDTH, GRID_HEIGHT, NUMBER_OF_CARS
+
+import math
 
 # Define a CanvasGrid to visualise the Road Network Model
 canvas_element = CanvasGrid(road_network_model_portrayal, GRID_WIDTH, GRID_HEIGHT, CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -24,15 +24,29 @@ chart = ChartModule(
 
 class InfoTextElement(TextElement):
     def render(self, model):
+        # Mean Travel Time
         mean_travel_time = model.mean_travel_time
         mean_travel_time_text = "{0:.2f}".format(mean_travel_time)
+
+        # Day
         day_text = str(model.day)
+
+        # Day Oddity
         oddity_text = ""
         if model.is_odd_date:
             oddity_text = "Odd day"
         else:
             oddity_text = "Even day"
-        return "Mean Travel Time: {}<br>Day: {}<br>Oddity: {}".format(mean_travel_time_text, day_text, oddity_text)
+
+        # Time
+        current_time = model.tick % 1440
+        hour = math.floor(model.tick / 60)
+        minute = model.tick % 60
+        hour_text = "0" + str(hour) if hour < 10 else str(hour)
+        minute_text = "0" + str(minute) if minute < 10 else str(minute)
+        time_text = hour_text + ":" + minute_text
+
+        return "Mean Travel Time: {}<br>Day: {} ({}) <br>Time: {}".format(mean_travel_time_text, day_text, oddity_text, time_text)
 
 policy_range_time = [
     '7_10_and_16_19',
